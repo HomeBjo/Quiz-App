44ebdfc20817afc12d3fc8b454d7429bf7d8541a
HomeBjo <lala2-wuppertal@hotmail.com>
2023-09-05 11:42:59 +0200

    test

---
+++ script.js
@@ -0,0 +1,59 @@
+let questions = [
+ {
+    "question": "Was ist die Hauptstadt von Burkina Faso?",
+    "answer_1": "Ouagadougou",
+    "answer_2": "Bamako",
+    "answer_3": "Abuja",
+    "answer_4": "Kigali",
+    "right_answer": 1
+  },
+  {
+    "question":
+      "Welche Programmiersprache wird oft für maschinelles Lernen und künstliche Intelligenz verwendet?",
+    "answer_1": "JavaScript",
+    "answer_2": "Python",
+    "answer_3": "Java",
+    "answer_4": "C++",
+    "right_answer": 2
+  },
+  {
+    "question":
+      "Welches Tier kann ohne Wasserüberleben, indem es Feuchtigkeit aus seiner Nahrung extrahiert und seinen Urin zu einem festen Zustand umwandelt?",
+    "answer_1": "Kamel",
+    "answer_2": "Pinguin",
+    "answer_3": "Frosch",
+    "answer_4": "Giraffe",
+    "right_answer": 1
+  },
+  {
+    "question": "Welches ist das schnellste Tier der Welt?",
+    "answer_1": "Giraffe",
+    "answer_2": "Elefant",
+    "answer_3": "Gepard",
+    "answer_4": "Pinguin",
+    "right_answer": 3
+  },
+  {
+    "question":
+      "In welchem Computerspiel sammelt man Ringe und bekämpft den bösen Dr. Robotnik?",
+    "answer_1": "Super Mario",
+    "answer_2": "The Legend of Zelda",
+    "answer_3": "Sonic the Hedgehog",
+    "answer_4": "Minecraft",
+    "right_answer": 3
+  },
+  {
+    "question": "Was ist JavaScript?",
+    "answer_1": "Eine Kaffeemarke",
+    "answer_2": "Eine Programmiersprache für Webentwicklung",
+    "answer_3": "Ein Computerspiel",
+    "answer_4": "Eine Vogelart",
+    "right_answer": 2
+  }
+];
+
+
+function init() {
+    
+    
+}
